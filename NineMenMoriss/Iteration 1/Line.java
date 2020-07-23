8e8343bd018fa68cac79226d11543f8977aeda6d
SrishtiPahel <68189064+SrishtiPahel@users.noreply.github.com>
2020-07-22 20:37:11 -0600

    Add threeInALine method

--- NineMenMoriss/Iteration 1/Line.java
+++ NineMenMoriss/Iteration 1/Line.java
@@ -1,150 +1,139 @@
+/**
+ * The Line class helps to get the position of all the points and keep track of
+ * the positions of the stones on the lines for all the players. This class can
+ * be used to consistently check if any of the players or the computer has three
+ * stones in a row. This class helps other classes to take appropriate action or
+ * print appropriate message based on the information gathered about the stones
+ * and their relative position on the board.
+ * 
+ * @author srish
+ *
+ */
 public class Line {
-	private String name;
+	private String playerName;// what does this variable represent?
 	private Point endPoint1;
 	private Point midPoint;
 	private Point endPoint2;
 	private boolean isFilled;
-	private Point[] points;
-	
+	private Point[] slotLocationOnBoard;
+
 	public Line(String name, Point endPoint1, Point midPoint, Point endPoint2) {
-		this.name = name;
+		this.playerName = name;
 		isFilled = false;
-		points = new Point[3];
-		
+		slotLocationOnBoard = new Point[3];// meaning of integer in []?
+
 		this.endPoint1 = endPoint1;
-		points[0] = this.endPoint1;
+		slotLocationOnBoard[0] = this.endPoint1;
 		this.midPoint = midPoint;
-		points[1] = this.midPoint;
+		slotLocationOnBoard[1] = this.midPoint;
 		this.endPoint2 = endPoint2;
-		points[2] = this.endPoint2;
+		slotLocationOnBoard[2] = this.endPoint2;
 	}
-	
+
 	public String toString() {
-		return name;
+		return playerName;// why is this method needed?, access player name from another class?
 	}
-	
-	public Point[] getPoints() {
-		return points;
+
+	public Point[] getPoints() {// Why is this Point [] only whereas other getter methods use Point only?
+		return slotLocationOnBoard;
 	}
-	
+
 	public Point getEndPoint1() {
 		return endPoint1;
 	}
-	
+
 	public Point getEndPoint2() {
 		return endPoint2;
 	}
-	
+
 	public Point getMidPoint() {
 		return midPoint;
 	}
 	
-	public boolean getFilled(Player player) {		
-		if (endPoint1.getOccupiedPlayer() != null) {
-			if (endPoint1.getOccupiedPlayer() == midPoint.getOccupiedPlayer()) {
-				if (midPoint.getOccupiedPlayer() == endPoint2.getOccupiedPlayer()) {
-					return true;
-				}
-				
-				else {
-					return false;
-				}
-			}
-			
-			else {
-				return false;
-			}		
-		}
-		
-		else {
-			return false;
-		}
-	
+	public void setFilled(boolean filled) {//Where do you use this?
+		isFilled = filled;
 	}
 
-	
-	public boolean doesLineContain(Stone stoneToCheck) {
-		boolean contains = false;
-		
-		if (endPoint1.getOccupiedStone() == stoneToCheck) {
-			contains = true;
+//	public boolean isLineFilled(Player player) {// check if all three points of a line are occupied for player
+//		if (endPoint1.getOccupiedStone() != null) {
+//			if (endPoint1.getOccupiedStone() == midPoint.getOccupiedPlayer()) {
+//				if (midPoint.getOccupiedStone() == endPoint2.getOccupiedPlayer()) {
+//					return true;
+//				}
+//
+//				else {
+//					return false;
+//				}
+//			}
+//
+//			else {
+//				return false;
+//			}
+//		}
+//
+//		else {
+//			return false;
+//		}
+//
+//	}
+
+	public void isLineFilled (Stone playerStonePosition) {
+		if (doesSlotContainStone(playerStonePosition)==3) {
+			isFilled = true;
+			System.out.println(playerName+"has three stones in a line.");
 		}
 		
-		else if (midPoint.getOccupiedStone() == stoneToCheck) {
-			contains = true;
-		}
-		
-		else if (endPoint2.getOccupiedStone() == stoneToCheck) {
-			contains = true;
-		}
-		
-		return contains;
 	}
-	
-	public boolean doesLineContainPoint(Point pointToCheck) {
-		boolean contains = false;
-		
-		if (endPoint1 == pointToCheck) {
-			contains = true;
+	public int doesSlotContainStone(Stone playerStonePosition) {// checks number of stones in a line
+		boolean slotContainsAStone = false;//purpose to check this, where is it used?
+		int numberOfStonesInLine = 0;
+
+		if (endPoint1.getOccupiedStone() == playerStonePosition) {
+			slotContainsAStone = true;
+			numberOfStonesInLine += 1;
+		}	
+			if (midPoint.getOccupiedStone() == playerStonePosition) {
+				slotContainsAStone = true;
+				numberOfStonesInLine += 1;
+			}
+				if (endPoint2.getOccupiedStone() == playerStonePosition) {
+					slotContainsAStone = true;
+					numberOfStonesInLine += 1;
+				}
+
+		if (midPoint.getOccupiedStone() == playerStonePosition) {
+			slotContainsAStone = true;
+			numberOfStonesInLine += 1;
+		}	
+			if (endPoint2.getOccupiedStone() == playerStonePosition) {
+			slotContainsAStone = true;
+			numberOfStonesInLine += 1;
+			}
+
+		else if (endPoint2.getOccupiedStone() == playerStonePosition) {
+			slotContainsAStone = true;
+			numberOfStonesInLine += 1;
 		}
-		
-		else if (midPoint == pointToCheck) {
-			contains = true;
-		}
-		
-		else if (endPoint2 == pointToCheck) {
-			contains = true;
-		}
-		
-		return contains;
+
+		return numberOfStonesInLine;
 	}
-	
-	
-	public void setFilled(boolean filled) {
-		isFilled = filled;
+
+	public boolean doesLineContainPoint(Point pointToBeChecked) {
+		boolean contains = false;
+
+		if (endPoint1 == pointToBeChecked) {
+			contains = true;
+		}
+
+		else if (midPoint == pointToBeChecked) {
+			contains = true;
+		}
+
+		else if (endPoint2 == pointToBeChecked) {
+			contains = true;
+		}
+
+		return contains;
 	}
 	
 }
-
-
-
-
-
-//
-//public class Line {
-//	private Slot[] slots;
-//	
-//	public Line(Slot slot1, Slot slot2, Slot slot3) {
-//		
-//		slots = new Slot[3];
-//		
-//		slots[0] = slot1;
-//		slots[1] = slot2;
-//		slots[2] = slot3;
-//	}
-//	
-//	public Slot[] getSlots() {
-//		return slots;
-//	}
-//	
-//	public boolean isInLine(Slot slotToCheck) {
-//		boolean answer = false;
-//		for (Slot eachSlot : slots) {
-//			
-//			if (eachSlot == slotToCheck) {
-//				answer = true;
-//			}
-//		}
-//		
-//		return answer;
-//	}
-//	
-//	public String toString() {
-//		String result = "";
-//		for (Slot eachSlot : slots) {
-//			result = result + eachSlot.toString();
-//		}
-//		
-//		return result;
-//	}
-//}
