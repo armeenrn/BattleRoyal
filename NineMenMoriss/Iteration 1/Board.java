c8e94b5637c955d99995fc699f9ee09b1a58fa64
armeenrn <67667209+armeenrn@users.noreply.github.com>
2020-07-22 19:29:31 -0600

    Updates to Board Class
    - We probably don't need the four midlines set as instance variables since they will be added to a square object anyways
    - list of squares as an instance variable
    - changed constructor. The board gets the center x and y coordinates and size of the board from the game class, and then creates thes quares using these variables.
    
    - Added display method to display points
    
    - Added a get point method so that board can return a point based on the coordinates given as argument

--- NineMenMoriss/Iteration 1/Board.java
+++ NineMenMoriss/Iteration 1/Board.java
@@ -1,33 +1,73 @@
 
-
 public class Board {
 	
-	private Square outerSquare;
-	private Square midSquare;
-	private Square innerSquare;
-	private Line northMidLine;
-	private Line westMidLine;
-	private Line eastMidLine;
-	private Line southMidLine;
+	private Square outerSquare;		
+	private Square midSquare;		
+	private Square innerSquare;	
 	
-	public Board (Square outerSquare, Square midSquare, Square innerSquare, Line northMidLine, Line westMidLine, Line eastMidLine, Line southMidLine) {
-		this.outerSquare = outerSquare;
-		this.midSquare = midSquare;
-		this.innerSquare = innerSquare;
-		this.northMidLine = northMidLine;
-		this.westMidLine = westMidLine;
-		this.eastMidLine = eastMidLine;
-		this.southMidLine = southMidLine;
-	}
+	/* probably won't need these above three, as they're already in squares list */	
+	
+	private Square[] squares;
+	
+	public Board(double range, double centerX, double centerY) {
+		squares = new Square[4];
+		
+		outerSquare = new Square("outer", range, centerX - range/2, centerY - range/2);
+		squares[0] = outerSquare;
+			
+		range = range * 0.75;
+			
+		midSquare = new Square("midSquare", range, centerX - range/2, centerY - range/2);
+		squares[1] = midSquare;
+			
+		range = range * 0.75;
+			
+		innerSquare = new Square("innersquare", range, centerX - range/2, centerY - range/2);
+		squares[2] = innerSquare;
+			
+		Line northMidLine = new Line("northMidLine", outerSquare.getNorthLine().getMidPoint(), midSquare.getNorthLine().getMidPoint(), innerSquare.getNorthLine().getMidPoint());
+		Line westMidLine = new Line("westMidLine", outerSquare.getWestLine().getMidPoint(), midSquare.getWestLine().getMidPoint(), innerSquare.getWestLine().getMidPoint());
+		Line eastMidLine = new Line("eastMidLine", outerSquare.getEastLine().getMidPoint(), midSquare.getEastLine().getMidPoint(), innerSquare.getEastLine().getMidPoint());
+		Line southMidLine = new Line("southMidLine", outerSquare.getSouthLine().getMidPoint(), midSquare.getSouthLine().getMidPoint(), innerSquare.getSouthLine().getMidPoint());	
+			
+			
+			
+		Square pseudoSquare = new Square(northMidLine, westMidLine, eastMidLine, southMidLine);
+		squares[3] = pseudoSquare;
+					
+		}
 
-	public Square[] setUpBoardSquares(){
-		Square[] setUp = {outerSquare, midSquare, innerSquare};
-		return Square[];
+	public Square[] getSquares(){
+		return squares;
+	}
+	
+	
+	
+	public Point getPointByCoordinates(double xcor, double ycor) {
+		Point answer = null;
+		for (Square eachSquare : squares) {
+			for (Line eachLine : eachSquare.getLines()) {
+					for (Point eachPoint : eachLine.getPoints()) {
+						if (eachPoint.getX() == xcor && eachPoint.getY() == ycor) {
+							answer = eachPoint;
+						}
+					}
+			}
+		}
+			
+		return answer;
+	}
+		
+
+
+	public void displayBoard() {
+		for (Square eachSquare : squares) {
+			for (Line eachLine : eachSquare.getLines()) {
+				for (Point eachPoint : eachLine.getPoints()) {
+					System.out.println(eachSquare.toString() + ": " + eachLine.toString() + ": " + eachPoint.toString());
+				}
+			}
 		}
 	}
-	
-	public Line[] setUpBoardLines() {
-		Line[] setUp = {northMidLine, westMidLine, eastMidLine, southMidLine};
-		return Line[];
-	}
+			
 }
