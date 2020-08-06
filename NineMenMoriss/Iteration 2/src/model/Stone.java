68a31af97b04672acfc66e7f1c2841c5d3228cc8
SrishtiPahel <68189064+SrishtiPahel@users.noreply.github.com>
2020-08-06 10:46:07 -0600

    Added documentation for stone class

--- NineMenMoriss/Iteration 2/src/model/Stone.java
+++ NineMenMoriss/Iteration 2/src/model/Stone.java
@@ -1,5 +1,12 @@
 package model;
 
+/**
+ * The class can be accessed to view details about any and all stones of any
+ * player.
+ * 
+ * @author srish
+ *
+ */
 public class Stone {
 	private Point location;
 	private Player owner;
@@ -9,35 +16,59 @@ public class Stone {
 	 * Constructor of a Stone object
 	 * 
 	 * @param location location of the Stone object
-	 * @param Player player the stone belongs to
+	 * @param Player   player the stone belongs to
 	 */
 	public Stone(Player owner) {
 		this.owner = owner;
 		isDead = false;
 	}
 
+	/**
+	 * Constructor of Stone object
+	 * @param owner owner of the stone
+	 * @param location location of the stone.
+	 */
 	public Stone(Player owner, Point location) {
 		this.owner = owner;
 		this.location = location;
 		isDead = false;
 	}
 
+	/**
+	 * Setter method to set location of where the stone will be placed.
+	 * @param location
+	 */
 	public void setLocation(Point location) {
-		this.location = location;	
+		this.location = location;
 	}
-	
+
+	/**
+	 * get the location of the stone as type point
+	 * @return location of type point 
+	 */
 	public Point getLocation() {
-		return location;	
+		return location;
 	}
-	
+
+	/**
+	 * Getter method to get player who the stone belongs to 
+	 * @return owner of the stone as player type 
+	 */
 	public Player getOwner() {
 		return owner;
 	}
-	
+
+	/**
+	 * Getter method for all stones that are dead (removed from the game by opponent)
+	 * @return boolean statement of true or false for a stone is dead
+	 */
 	public boolean getDead() {
 		return isDead;
 	}
-	
+
+	/**
+	 * Setter method for to set a stone as dead if removed from the game
+	 */
 	public void setDead() {
 		isDead = true;
 	}
