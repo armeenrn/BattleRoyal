6c1dc20ad36fb763ab9dbd975e9b8d6b1cb09c26
sadiasaad1 <sadia.saad1@ucalgary.ca>
2020-08-10 19:00:03 -0600

    Revert "Removing redundant code"
    This reverts commit 4ea50b1abbdf4f197d5409061aa624ca7db2af44.

--- NineMenMoriss/Iteration 2/src/model/Board.java
+++ NineMenMoriss/Iteration 2/src/model/Board.java
@@ -7,11 +7,10 @@ package model;
  *
  */
 public class Board {
-/*
+
 	private Square outerSquare;
 	private Square midSquare;
 	private Square innerSquare;
-	*/
 	private Line northMidLine;
 	private Line westMidLine;
 	private Line eastMidLine;
@@ -31,25 +30,25 @@ public class Board {
 	public Board(double range, double centreXCoord, double centreYCoord) {
 		squares = new Square[4];
 
-		squares[0] = new Square("Outer Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
-		//squares[0] = outerSquare;
+		outerSquare = new Square("Outer Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
+		squares[0] = outerSquare;
 		range = range * (2.0 / 3);
 
-		squares[1] = new Square("Middle Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
-		//squares[1] = midSquare;
+		midSquare = new Square("Middle Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
+		squares[1] = midSquare;
 		range = range * 0.5;
 
-		squares[2] = new Square("Inner Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
-		//squares[2] = innerSquare;
+		innerSquare = new Square("Inner Square", range, centreXCoord - range / 2, centreYCoord - range / 2);
+		squares[2] = innerSquare;
 
-		northMidLine = new Line("North Mid Line", squares[2].getLines()[3].getPoints()[1],
-				squares[1].getLines()[3].getPoints()[1], squares[0].getLines()[3].getPoints()[1]);
-		westMidLine = new Line("West Mid Line", squares[2].getLines()[1].getPoints()[1],
-				squares[1].getLines()[1].getPoints()[1], squares[0].getLines()[1].getPoints()[1]);
-		eastMidLine = new Line("East Mid Line", squares[2].getLines()[2].getPoints()[1],
-				squares[1].getLines()[2].getPoints()[1], squares[0].getLines()[2].getPoints()[1]);
-		southMidLine = new Line("South Mid Line", squares[2].getLines()[0].getPoints()[1],
-				squares[1].getLines()[0].getPoints()[1], squares[0].getLines()[0].getPoints()[1]);
+		northMidLine = new Line("North Mid Line", innerSquare.getNorthLine().getMidPoint(),
+				midSquare.getNorthLine().getMidPoint(), outerSquare.getNorthLine().getMidPoint());
+		westMidLine = new Line("West Mid Line", innerSquare.getWestLine().getMidPoint(),
+				midSquare.getWestLine().getMidPoint(), outerSquare.getWestLine().getMidPoint());
+		eastMidLine = new Line("East Mid Line", innerSquare.getEastLine().getMidPoint(),
+				midSquare.getEastLine().getMidPoint(), outerSquare.getEastLine().getMidPoint());
+		southMidLine = new Line("South Mid Line", innerSquare.getSouthLine().getMidPoint(),
+				midSquare.getSouthLine().getMidPoint(), outerSquare.getSouthLine().getMidPoint());
 		Square linesOnBoard = new Square(northMidLine, westMidLine, eastMidLine, southMidLine);
 		squares[3] = linesOnBoard;
 	}
@@ -69,10 +68,10 @@ public class Board {
 	 */
 	public void displayBoard() {
 		for (int i = 0; i < squares.length; i++) {
-			System.out.println("Points on the North Lines: " + squares[i].getLines()[3].getPoints());
-			System.out.println("Points on the East Lines: " + squares[i].getLines()[2].getPoints());
-			System.out.println("Points on the West Lines: " + squares[i].getLines()[1].getPoints());
-			System.out.println("Points on the South Lines: " + squares[i].getLines()[0].getPoints());
+			System.out.println("Points on the North Lines: " + squares[i].getNorthLine().getPoints());
+			System.out.println("Points on the East Lines: " + squares[i].getEastLine().getPoints());
+			System.out.println("Points on the West Lines: " + squares[i].getWestLine().getPoints());
+			System.out.println("Points on the South Lines: " + squares[i].getSouthLine().getPoints());
 		}
 	}
 
